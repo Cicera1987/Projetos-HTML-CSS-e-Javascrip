5f3c2d4ccd82bdc5cda40843ac451c4152fb999f
Cicera Ribeiro <maccicera@gmail.com>
2022-06-07 13:45:23 -0300

    trabalhando variaveis

---
+++ Curso - Javascript/assets/js/concatenacao.js
@@ -0,0 +1,24 @@
+
+var nome = 'Cicera';
+var idade = 35;
+
+var nome1 = prompt('Digite o seu nome:');
+var idade1 = prompt('Digite sua idade:');
+var idade2 = 50;
+
+
+console.log(idade1);
+console.log(idade2);
+
+document.write('<h1>Olá ' + nome1 + ', tudo bem? Estou vendo aqui que você possui '+ idade1 +', anos </h1>');
+
+//Valores Null e Undefined
+//Null
+var test = null
+
+
+//Andefined
+var teste2 //undefined
+
+console.log(test)
+console.log(teste2)
---
+++ Curso - Javascript/assets/js/index.js
@@ -0,0 +1,34 @@
+//alert('Olá, seja bem vindo ao curso')
+
+/*
+
+    Baixo nos temos um elemento no DOM (pagina HTML)
+    Seu papel é atualizar o valor desse elemento com uma string
+
+*/
+
+//document.getElementById('nome').value = 'Oi'
+
+
+//String  -  responsaveis por armazenar os caracteres em texto
+var texto = "Curso de javascript"
+
+
+
+//Number  -  responsaveis por armezenar e atribuir os dsdos numéricos
+var numeroInteiro = -7
+var numeroFracionado = 11210.75
+
+
+
+//Boolean  -  responsaveis por testes condicionais
+var teste = true // mostra se o valor é verdadeiro
+var teste = false // mostra se e teste é falso
+
+//comandos
+
+alert(numeroFracionado) //abrir um dialog - mostrando o valor da variavel
+
+document.write(texto) // este documento é inserido diretamente no console.
+
+console.log(numeroInteiro)
