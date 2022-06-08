9e450994f68a069235cfed6519549bd2cbb23d14
Cicera Ribeiro <maccicera@gmail.com>
2022-06-08 14:27:44 -0300

    mais comunados

---
+++ Javascript - Funcao/assets/js/index.js
@@ -0,0 +1,26 @@
+
+// função pode se void
+// função do tipo return
+
+function calcularArea ( largura, comprimento ){
+
+    //logica
+    var area = largura * comprimento
+
+    return area
+
+}
+
+largura = parseFloat(largura)
+comprimento = parseFloat(comprimento)
+
+
+var largura = prompt(' Informe a largura do terreno em metros')
+var comprimento = prompt(' Informe o comprimento do terreno em metros')
+
+
+var are = calcularArea(largura, comprimento)
+
+document.write('O terreno possui ' + are + ' metros quadrados')
+
+
