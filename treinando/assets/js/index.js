767944bcaf5eedffcc4c46dc13987422badf0b4d
Cicera Ribeiro <maccicera@gmail.com>
2022-06-09 13:35:31 -0300

    novas atualzações

---
+++ treinando/assets/js/index.js
@@ -0,0 +1,9 @@
+// if else if   e else
+
+const num = 10
+if (num >= 0 && num <= 5){
+console.log('Sim ', +num+ ' é maior igua a zero')
+
+} else{
+    console.log( num + ' não está em zero e 5')
+}
