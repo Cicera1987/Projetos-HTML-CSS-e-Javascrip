b63a55f7938f84a9c514f1184b429a163e9430d8
Cicera Ribeiro <maccicera@gmail.com>
2022-06-17 08:33:43 -0300

    enviando

---
+++ Treinamentos Diarios/Exercicios - Objetos/03 Exercicio/assets/js/index.js
@@ -0,0 +1,24 @@
+//Sessão de exercicios de Orientação e objetos
+
+//01 - Crie uma classe que simula uma conta no banco.
+//Utilize a forma que prefirir
+// Deve conter a propriedade - saldo;
+//E os metodos deposito e saque;
+//Teste os métodos
+class Conta {
+    constructor(saldo){
+        this.saldo = saldo;
+
+    }
+
+    deposito(valor){
+        this.saldo += valor;
+    }
+
+}
+
+let conta = new Conta(1000);
+
+conta.deposito(1000);
+
+console.log(conta.saldo)
